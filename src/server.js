85d8e728e36f3fa5da83712245e392e5b185860e
Sanjeet Mijar <ssnjitm6@gmail.com>
2026-04-27 17:49:13 +0545

    fix3

--- src/server.js
+++ src/server.js
@@ -3,9 +3,7 @@ import { app } from "./app.js";
 import connectDB from "./config/db.config.js";
 
 dotenv.config({
-    path: "./.env"
-
-})
+});
 const PORT =process.env.PORT || 7860;
 
 // // Only run seed in development or if explicitly requested
