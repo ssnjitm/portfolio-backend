769f77e8d758b1b9b8a829fadfea05949f690c3d
Sanjeet Mijar <ssnjitm6@gmail.com>
2026-06-07 08:40:18 +0545

    prod fix:1

--- src/app.js
+++ src/app.js
@@ -31,6 +31,21 @@ app.get("/api/v1/test", (req, res) => {
     res.json({ message: "Backend is reachable!" });
 });
 
+// Global error handler
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    const errors = err.errors || [];
+    console.error('API error:', err);
+
+    res.status(statusCode).json({
+        statusCode,
+        success: false,
+        message,
+        errors,
+        data: err.data ?? null,
+    });
+});
 
 export default app;
 export { app };
